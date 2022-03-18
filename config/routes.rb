b458fe2bdfe4fcb89a451630e437c8d600032740
Patech_Patrice <patricedrayton86@gmail.com>
2022-03-18 13:31:48 -0400

    added routes for category items

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   #root "homepage#index"
+  resources :handbags
+  resources :shoes
+  resources :tops
+  resources :jackets
+  resources :bottoms
+  resources :accessories
 end
